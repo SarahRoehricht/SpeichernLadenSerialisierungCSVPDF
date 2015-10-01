c8d0ffdd8f754d327af93a82b26989bfcb9050fe
Alfred Loran <loranalf@googlemail.com>
2015-10-01 20:15:28 +0200

    Klasse Student angelegt und fertig implementiert

---
+++ src/personenverwaltung/Student.java
@@ -0,0 +1,92 @@
+package personenverwaltung;
+
+/**
+ * Diese Klasse bildet einen Studenten ab.
+ * @author Alfred Loran
+ * @version 1.0.0
+ */
+class Student extends Person {
+	private String matrikelNummer;
+	private Vorlesung vorlesung;
+	
+	/**
+	 * Erzeugt ein Student - Objekt mit den uebergebenen Parametern.
+	 * @param vorName Der uebergebene Vorname.
+	 * @param nachName Der uebergebene Nachname.
+	 * @param strasse Die uebergebeneStrasse.
+	 * @param hausNummer Die uebergebene Hausnummer.
+	 * @param ort Der uebergebene Ort.
+	 * @param plz Die uebergebene Postleitzahl.
+	 * @since 1.0.0
+	 */
+	public Student(String vorName, String nachName, String strasse, 
+	               int hausNummer, String ort, String plz) {
+		super(vorName,nachName,strasse,hausNummer,ort,plz);
+		erzeugeMatrikelNummer();
+	}
+	
+	/**
+	 * Erzeugt die Matrikelnummer.
+	 * @since 1.0.0
+	 */
+	private void erzeugeMatrikelNummer() {
+		this.matrikelNummer = getName().getVorName().substring(0,1) +
+							  getName().getNachName().substring(0, 1) +
+							  getAdresse().getOrt().substring(0,1);
+	}
+	
+	/**
+	 * Liefert die Matrikelnummer.
+	 * @return die Matrikelnummer.
+	 * @since 1.0.0
+	 */
+	protected String getMatrikelNummer() {
+		return matrikelNummer;
+	}
+	
+	/**
+	 * Lässt den Studenten an der Vorlesung teilnehmen.
+	 * @param vorlesung Die uebergebene Vorlesung.
+	 * @since 1.0.0
+	 */
+	protected void nimmtAnVorlesungTeil(Vorlesung vorlesung) {
+		this.vorlesung = vorlesung;
+		vorlesung.fuegeStudentHinzu(this);
+	}
+	
+	/**
+	 * Liefert die Daten des Objektes.
+	 * @return die Daten des Objektes.
+	 * @since 1.0.0
+	 */
+	@Override
+	public String toString() {
+		String ausgabe = "";
+		ausgabe += "Matrikelnummer: "+ getMatrikelNummer() + "\n" +
+				super.toString() + "\n";
+		
+		if(this.vorlesung != null) {
+			ausgabe += "Nimmt an Vorlesung \n" + vorlesung.toString() + "\nteil.";
+		}
+		return ausgabe;
+	}
+	
+	/**
+	 * Vergleicht zwei Objekte.
+	 * @param Das zu vergleichende Objekt.
+	 * @return true, Objekte sind gleich.
+	 * 		   false, Objekte sind ungleich.
+	 * @since 1.0.0
+	 */
+	public boolean equals(Object object) {
+		if(object == null) {
+			throw new NullPointerException("Kein Objekt zum Vergleichen vorhanden!");
+		} else if(!(object instanceof Student)) {
+			return false;
+		} else {
+			Student s = (Student)object;
+			boolean isGleich = super.equals(object);
+			return (s.getMatrikelNummer().equals(this.getMatrikelNummer()) && isGleich);
+		}
+	}	
+}
