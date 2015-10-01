1b26e302829bbfa010b1b25591722a0177a11040
Alfred Loran <loranalf@googlemail.com>
2015-10-01 21:26:11 +0200

    Testklasse fertig geschrieben. Backend fertig. Bisher keine Bugs aufgefallen

---
+++ src/TestPersonenVerwaltung.java
@@ -0,0 +1,32 @@
+import interfaces.iDozent;
+import interfaces.iStudent;
+import personenverwaltung.PersonenVerwaltung;
+
+/**
+ * Diese Klasse bildet die Testklasse fuer das Programm ab.
+ * @author Alfred Loran
+ * @version 1.0.0
+ */
+public class TestPersonenVerwaltung {
+	/**
+	 * Main - Methode.
+	 * @param args Die uebergebenen Argumente.
+	 * @since 1.0.0
+	 */
+	public static void main(String[] args) {
+		iDozent dozent;
+		iStudent student;
+		PersonenVerwaltung pV = new PersonenVerwaltung();
+		dozent = pV; student = pV;
+		dozent.erstelleDozenten("Frank", "Dopatka", "Musterstrasse", 1, "Musterort", "12345");
+		dozent.erstelleVorlesung("Informatik 2");
+		System.out.println(dozent);
+		student.erzeugeStudenten("Alfred", "Loran", "Geht euch n scheiss an", 10, "Reutlingen", "72762");
+		System.out.println(student);
+		student.nehmeAnVorlesungTeil("Informatik 2");
+		System.out.println(student);
+		dozent.trageNoteEin(1.3);
+		System.out.println(dozent);
+	}
+
+}
