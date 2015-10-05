e525e14fda7e0c96ec018344a4975f3f7b65ba96
Alfred Loran <loranalf@googlemail.com>
2015-10-05 14:54:44 +0200

    Interface SpeichernLadenCSV erstellt

---
+++ src/interfaces/iSpeichernLadenCSV.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+/**
+ * Dieses Interface beinhaltet die Methoden zum Speichern der CSV.
+ * @author Alfred Loran
+ * @version 1.0.0
+ */
+public interface iSpeichernLadenCSV {
+	/**
+	 * Diese Methode speichert die Daten in eine CSV.
+	 * @param dateiName Der uebergebene Dateiname.
+	 * @since 1.0.0
+	 */
+	public void speichernCSV(String dateiName);
+}
