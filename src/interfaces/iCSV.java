7a328196d697faaf5f202dee30d0c0842b666be3
Alfred Loran <loranalf@gmx.de>
2015-10-07 23:35:59 +0200

    Interface iCSV created

---
+++ src/interfaces/iCSV.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+public interface iCSV {
+	/**
+	 * This method saves the data in a CSV file.
+	 * @param fileName The passed file name.
+	 * @since 1.0.0
+	 */
+	public void saveCSV(String fileName);
+	
+	/**
+	 * Loads the objects in a CSV file.
+	 * @param fileName The passed Filename.
+	 * @return the loaded objects.
+	 * @since 1.0.0
+	 */
+	public Object loadCSV(String fileName);
+}
